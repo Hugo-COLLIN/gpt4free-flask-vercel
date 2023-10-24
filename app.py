51e2400df8b882c98e17c1037c8cfcc659da4acd
Hugo COLLIN <hugo.collin1@outlook.fr>
2023-10-24 12:31:52 +0200

    Configuration for Vercel deployment

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask_cors import CORS
+from g4f import ChatCompletion
+
+app = Flask(__name__)
+CORS(app, origins=['http://localhost:5173', 'http://example.com'])
+
+
+@app.route('/api/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    response = ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[{"role": "user", "content": data['message']}],
+        stream=True,
+    )
+    return {'response': list(response)}
